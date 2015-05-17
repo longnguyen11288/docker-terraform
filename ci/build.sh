8ab07db6d4a1f0a935954a6e33c59684cc988c3e
Long Nguyen <long.nguyen11288@gmail.com>
2015-05-17 18:19:19 -0400

    updated to use concourse image

--- ci/build.sh
+++ ci/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TERRAFORM_VERSION=$(cat terraform-release/version)
+TERRAFORM_VERSION=0.5.0
 
 url="https://dl.bintray.com/mitchellh/terraform/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
 curl -L -o terraform-${TERRAFORM_VERSION}-linux-x86_64.zip ${url}
