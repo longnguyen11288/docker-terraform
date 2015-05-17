8cd4383db23a991eacdba617d8f3bb35084ce44e
Long Nguyen <long.nguyen11288@gmail.com>
2015-05-17 18:26:06 -0400

    use wget instead

--- ci/build.sh
+++ ci/build.sh
@@ -3,8 +3,8 @@
 TERRAFORM_VERSION=$(cat terraform-release/version)
 
 url="https://dl.bintray.com/mitchellh/terraform/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
-curl -L -o terraform-${TERRAFORM_VERSION}-linux-x86_64.zip ${url}
+wget ${url}
 
 mkdir -p bin
 
-unzip terraform-${TERRAFORM_VERSION}-linux-x86_64.zip -d bin/
+unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d bin/
