38bcbc8a0d73d0198f0e32c04c0183979c08a826
Long Nguyen <long.nguyen11288@gmail.com>
2015-05-17 18:50:51 -0400

    use tar instead of unzip

--- ci/build.sh
+++ ci/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-TERRAFORM_VERSION=0.5.0
+TERRAFORM_VERSION=$(cat terraform-release/version)
 
 url="https://dl.bintray.com/mitchellh/terraform/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
 curl -L -o terraform-${TERRAFORM_VERSION}-linux-x86_64.zip ${url}
 
 mkdir -p bin
 
-unzip terraform-${TERRAFORM_VERSION}-linux-x86_64.zip -d bin/
+tar xzf terraform-${TERRAFORM_VERSION}-linux-x86_64.zip -C bin/
